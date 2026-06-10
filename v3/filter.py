def132bf329724ad800388f804860359e36278f2
Cabbie69 <pkmohamed22@gmail.com>
2026-06-10 17:18:05 +0300

    Add job title filter with configurable keyword matching for V3 alerts

---
+++ v3/filter.py
@@ -0,0 +1,17 @@
+import json
+
+
+
+with open("config.json", "r") as f:
+    config = json.load(f)
+
+wanted_keywords = config["wanted_keywords"]
+not_wanted_keywords = config["not_wanted_keywords"]
+
+def job_passes_filter(job):
+    if any(keyword in job['title'].lower() for keyword in not_wanted_keywords):
+        return False
+    if any(keyword in job['title'].lower() for keyword in wanted_keywords):
+        return True
+    else:
+        return False
--- v3/poller.py
+++ v3/poller.py
@@ -2,6 +2,7 @@ from scraper import extract
 from redis_client import is_seen, mark_seen
 import time
 from notifier import send_new_job_alert
+from filter import job_passes_filter
 
 def poll_once():
     jobs = extract()
@@ -10,6 +11,9 @@ def poll_once():
             continue
         else:
             mark_seen(job["job_id"])
+            if not job_passes_filter(job):
+                print(f'SKIPPED : {job["title"]}')
+                continue
             print(f'NEW : {job["title"]}')
             send_new_job_alert(job)
     
